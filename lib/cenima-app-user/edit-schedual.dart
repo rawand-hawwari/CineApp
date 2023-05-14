93aff08503ca1cdb5514dd992ff9c92e7bb5ffbd
rawand-hawwari <rawandh3@gmail.com>
2023-05-14 18:07:15 +0300

    ...

--- lib/cenima-app-user/edit-schedual.dart
+++ lib/cenima-app-user/edit-schedual.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/cenima-app-user/admin-Home-page.dart';
-import 'package:myapp/services/schedual.dart';
 import 'package:myapp/utils.dart';
-import '../services/Showing now.dart' as global;
+
 import '../shared/Theme.dart';
 
 class EditMovieSchedual extends StatefulWidget {
@@ -20,7 +19,7 @@ class _EditMovieSchedual extends State<EditMovieSchedual> {
     "Screen 2",
     "Screen 3",
   ];
-  String dropdownVal = 'Mon, 22 May';
+  List<String> dropdownVal = [];
   List<String> age = [
     'Mon, 22 May',
     'Tue, 23 May',
@@ -30,14 +29,23 @@ class _EditMovieSchedual extends State<EditMovieSchedual> {
     'Sat, 27 May',
     'Sun, 28 May',
   ];
-  List<Widget> gridChild = [];
-  //   Container(
-  //     margin: EdgeInsets.all(8.0),
-  //     width: 10,
-  //     height: 10,
-  //     color: Colors.green,
-  //   ),
-  // ];
+  List<List<Widget>> gridChild = [[]];
+  List<String> dropdownTimeVal = [];
+  List<String> timeperiod = [
+    'AM',
+    'PM',
+  ];
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < screens.length; i++) {
+      setState(() {
+        dropdownVal.add("");
+        dropdownTimeVal.add("");
+        gridChild.add([]);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +54,6 @@ class _EditMovieSchedual extends State<EditMovieSchedual> {
 
     double baseWidth = 393;
     double fem = MediaQuery.of(context).size.width / baseWidth;
-    double ffem = fem * 0.97;
     return Scaffold(
       body: NestedScrollView(
         floatHeaderSlivers: true,
@@ -110,7 +117,8 @@ class _EditMovieSchedual extends State<EditMovieSchedual> {
                   ),
                 ),
                 // scheduals for screens list
-                const EditSchedual(),
+                // const EditSchedual(),
+                screenListBuilder(),
                 // Container(
                 //   child: ListView(
                 //     children: screens.map((strone) {
@@ -1098,6 +1106,198 @@ class _EditMovieSchedual extends State<EditMovieSchedual> {
     //     ),
     //   ),
     // );
+  }
+
+  Widget screenListBuilder() {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    final TextEditingController controller = TextEditingController();
+
+    return SizedBox(
+      height: height * 0.8,
+      child: ListView(
+        children: screens.map((strone) {
+          setState(() {});
+          return Card(
+            margin: EdgeInsets.zero,
+            shape: const RoundedRectangleBorder(
+              side: BorderSide(
+                color: Color(0xff707070),
+                width: 1,
+              ),
+            ),
+            child: ExpansionTile(
+              initiallyExpanded: false,
+              iconColor: mainColor,
+              title: Text(
+                strone,
+                style: SafeGoogleFont(
+                  'Segoe UI',
+                  height * 0.028,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              children: <Widget>[
+                const Padding(padding: EdgeInsets.all(5)),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //date dropdown
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Icon(Icons.date_range),
+                          DropdownButton(
+                            value: dropdownVal[screens.indexOf(strone)] == ""
+                                ? "Mon, 22 May"
+                                : dropdownVal[screens.indexOf(strone)],
+                            items: age.map((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: GoogleFonts.lato(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                      color: const Color(0xFF323232)),
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (String? value) {
+                              setState(() {
+                                if (value == null) {
+                                  dropdownVal[screens.indexOf(strone)] =
+                                      'Mon, 22 May';
+                                } else {
+                                  dropdownVal[screens.indexOf(strone)] = value;
+                                }
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      //add new time button
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: TextButton(
+                          onPressed: () {
+                            setState(() {
+                              gridChild[screens.indexOf(strone)].add(
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    SizedBox(
+                                      width: width * 0.2,
+                                      height: 30,
+                                      child: TextField(
+                                        keyboardType: const TextInputType
+                                            .numberWithOptions(decimal: true),
+                                        inputFormatters: [
+                                          FilteringTextInputFormatter.allow(
+                                              RegExp(r'^\d{0,2}\:?\d{0,2}')),
+                                        ],
+                                        controller: controller,
+                                        decoration: const InputDecoration(
+                                          border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(50)),
+                                          ),
+                                          filled: true,
+                                          fillColor: Color(0xFFFFFFFF),
+                                          hintText: '00:00',
+                                          contentPadding: EdgeInsets.all(5),
+                                          hintStyle: TextStyle(fontSize: 14),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: width * 0.2,
+                                      child: DropdownButton(
+                                        value: dropdownTimeVal[
+                                                    screens.indexOf(strone)] ==
+                                                ""
+                                            ? "AM"
+                                            : dropdownTimeVal[
+                                                screens.indexOf(strone)],
+                                        items: timeperiod.map((String value) {
+                                          return DropdownMenuItem<String>(
+                                            value: value,
+                                            child: Text(
+                                              value,
+                                              style: GoogleFonts.lato(
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.w400,
+                                                  color:
+                                                      const Color(0xFF323232)),
+                                            ),
+                                          );
+                                        }).toList(),
+                                        onChanged: (String? value) {
+                                          setState(() {
+                                            if (value == null) {
+                                              dropdownTimeVal[screens
+                                                  .indexOf(strone)] = 'AM';
+                                            } else {
+                                              dropdownTimeVal[screens
+                                                  .indexOf(strone)] = value;
+                                            }
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            });
+                          },
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            decoration: BoxDecoration(
+                              color: const Color(0xffff2153),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Add New Time",
+                                textAlign: TextAlign.center,
+                                style: SafeGoogleFont(
+                                  'Lucida Bright',
+                                  width * 0.04,
+                                  fontWeight: FontWeight.w400,
+                                  color: const Color(0xffffffff),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: width * 0.9,
+                  height: height * 0.2,
+                  child: GridView.count(
+                    childAspectRatio: height / width,
+                    crossAxisCount: 2,
+                    children: List.generate(
+                        gridChild[screens.indexOf(strone)].length,
+                        (index) => gridChild[screens.indexOf(strone)][index]),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
+    );
   }
 
   // Widget screenListBuilder() {
